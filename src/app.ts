404bb5cc8a3f626f3a7c8a3dcc12a00493f7f8f8
Valéria Todorovski Lucht de  Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-05-27 23:59:13 -0300

    Config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
-app.use(express.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+app.use(bodyParser.json());
 app.use(routes);
 
 export default app;
